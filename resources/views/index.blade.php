40c1dd89b1543497b774e8970ee4b6e8f917a47a
chilliesdev <matthewkayode116@gmail.com>
2020-08-20 14:46:23 +0100

    initial commit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -12,9 +12,23 @@
 
         <link rel="stylesheet" href="{{URL::asset('css/app.css')}}">
         {{-- <script src="https://js.paystack.co/v1/inline.js"></script>  --}}
+        <script src="{{URL::asset('js/app.js')  }}" defer></script>
+        {{-- <style>
+            .js-cookie-consent{
+                position: fixed;
+                bottom: 0;
+                padding: 2rem;
+                text-align: center;
+                width: 100%;
+                z-index: 4;
+                background-color: #ffbcdb;
+                border-color: #fffacc;
+                border: solid 1px;
+            }
+        </style> --}}
     </head>
     <body>
-        @include('cookieConsent::index')
+        {{-- @include('cookieConsent::index') --}}
         <div id="root"></div>
     </body>
 </html>
